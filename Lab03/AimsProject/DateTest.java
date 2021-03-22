b841e6097ff1bb009af9f8de542bf4e93cfb1797
sharparc-prog <80028577+sharparc-prog@users.noreply.github.com>
2021-03-23 04:27:08 +0700

    Update DateTest.java

--- Lab03/AimsProject/DateTest.java
+++ Lab03/AimsProject/DateTest.java
@@ -8,14 +8,14 @@ public class DateTest {
 		MyDate newDate = new MyDate();
 		newDate.print(newDate);
 		int i = 0;
+		System.out.println("Date format: mm/dd/yyyy or mm dd yyyy");
+		System.out.println("Example: March 27th 2020 or January 15 2020 or 03 15 2020");
+		System.out.println("Please enter a date: ");
 		do {
-			System.out.println("Please enter a date: ");
-			System.out.println("Date format: mm/dd/yyyy or mm dd yyyy");
-			System.out.println("Example: March 27th 2020 or January 15 2020 or 03 15 2020");
 			String date = keyboard.nextLine();
 			newDate.accept(date);
 			i = newDate.checkValid(newDate);
-			if (i == -1) {
+			if (i != 1) {
 				System.out.println("Invalid date! Please try again!");
 			}
 		} while (i != 1);
